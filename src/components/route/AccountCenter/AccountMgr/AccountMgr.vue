361f2cbb2763b8aa563f93cad37a81c7c4033fcf
diaoxinrui <443135581@qq.com>
2018-01-11 10:12:31 +0800

    11

--- src/components/route/AccountCenter/AccountMgr/AccountMgr.vue
+++ src/components/route/AccountCenter/AccountMgr/AccountMgr.vue
@@ -2,18 +2,26 @@
     <div class="AccountMgr">
         <div class="imgContainer">
             <div class="titleImg">
-                <div class="bank" :style='{backgroundImage:`url(${langImg})`}'></div>
-                <div class="lang" :style='{backgroundImage:`url(${bankImg})`}'></div>
+                <div class="bank"
+                    :style='{backgroundImage:`url(${langImg})`}'></div>
+                <div class="lang"
+                    :style='{backgroundImage:`url(${bankImg})`}'></div>
             </div>
             <div class="text">本界面将导航到民生银行提供的专属账户管理界面！</div>
             <div class="text">本功能2018年1月11号将与银行进行对接。2018年1月11号之后才能签约。</div>
             <div class="btn">
-                <el-button @click="goMgr" size="small" type="primary">账户管理</el-button>
+                <el-button @click="goMgr"
+                    size="small"
+                    type="primary">账户管理</el-button>
             </div>
         </div>
-        
-        <form v-show="false" id="bankForm" method="post" action="http://111.205.207.144:7003/ecp/htmlMhtInFwd/forward">
-            <button id="bankFormSubmit" type="submit">提交</button>
+
+        <form v-show="false"
+            id="bankForm"
+            method="post"
+            action="http://111.205.207.144:7003/ecp/htmlMhtInFwd/forward">
+            <button id="bankFormSubmit"
+                type="submit">提交</button>
         </form>
     </div>
 </template>
@@ -37,26 +45,29 @@ export default {
             _this.$http.post('/ocm-web/api/cmbc/queryCustomerInfo', params)
                 .then(res => {
                     debugger
-                    let bankForm = document.querySelector('#bankForm');
-                    let bankFormSubmit = document.querySelector('#bankFormSubmit');
-                    let params = res.data.reduce((acc, v) => {
-                        acc[v.name] = v.value;
-                        return acc;
-                    }, {});
-                    Object.keys(params).forEach(key => {
-                        if (key != 'forwardUrl') {
-                            let input = document.createElement('input');
-                            input.setAttribute('name', key);
-                            input.value = params[key];
-                            bankForm.appendChild(input);
-                        }
-                    });
-                    //url  地址
-                    debugger
-                    let forwardUrl = params.forwardUrl;
-                    bankForm.setAttribute('action', forwardUrl);
-                    document.forms.bankForm.submit();
-                    // bankFormSubmit.click();
+                    if (res.headers['x-ocm-code']== '1') {
+                        let bankForm = document.querySelector('#bankForm');
+                        let bankFormSubmit = document.querySelector('#bankFormSubmit');
+                        let params = res.data.reduce((acc, v) => {
+                            acc[v.name] = v.value;
+                            return acc;
+                        }, {});
+                        Object.keys(params).forEach(key => {
+                            if (key != 'forwardUrl') {
+                                let input = document.createElement('input');
+                                input.setAttribute('name', key);
+                                input.value = params[key];
+                                bankForm.appendChild(input);
+                            }
+                        });
+                        //url  地址
+                        debugger
+                        let forwardUrl = params.forwardUrl;
+                        bankForm.setAttribute('action', forwardUrl);
+                        document.forms.bankForm.submit();
+                        // bankFormSubmit.click();
+                    }
+
                 });
         },
         goAssign() {
