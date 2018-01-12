3835ca5642261da01ac6037ae2ed4a00842c8812
diaoxinrui <443135581@qq.com>
2018-01-12 13:18:44 +0800

    dd

--- src/components/route/ContactUs/ContactUs.vue
+++ src/components/route/ContactUs/ContactUs.vue
@@ -22,7 +22,7 @@
             description="">
         </el-alert>
 
-        <el-menu>
+        <!-- <el-menu>
             <el-submenu index="1">
                 <el-menu-item-group>
                     <template slot="title">分组一</template>
@@ -30,7 +30,7 @@
                     <el-menu-item index="1-2">选项2</el-menu-item>
                 </el-menu-item-group>
             </el-submenu>
-        </el-menu>
+        </el-menu> -->
 
     </div>
 </template>
