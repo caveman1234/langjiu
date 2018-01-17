555056ec81a71aafaed0b9e9cd64d82038754393
diaoxinrui <443135581@qq.com>
2018-01-17 10:28:10 +0800

    电话

--- src/components/route/ContactUs/ContactUs.vue
+++ src/components/route/ContactUs/ContactUs.vue
@@ -1,25 +1,16 @@
 <template>
     <div class="ContactUs">
         <div class="title">欢迎致电四川古蔺郎酒销售有限公司</div>
-        <el-alert title="青花郎经销商客服电话：028-86719513、028-86726355、028-86710905"
-            type="info"
-            :closable="false"
-            description="">
+
+        <el-alert title="请您在以下时间拨打客服热线" type="info" :closable="false" description="工作日（Mon.—Fri.）早上9:00—下午5:00">
         </el-alert>
-        <el-alert title="小郎酒经销商客服电话：028-86651839、028-86665349"
-            type="info"
-            :closable="false"
-            description="">
+        <el-alert title="青花郎经销商客服电话：028-86719513、028-86726355、028-86710905" type="info" :closable="false" description="">
         </el-alert>
-        <el-alert title="郎牌特曲经销商客服电话：028-86652305、028-86730207"
-            type="info"
-            :closable="false"
-            description="">
+        <el-alert title="小郎酒经销商客服电话：028-86651839、028-86665349" type="info" :closable="false" description="">
         </el-alert>
-        <el-alert title="全国统一服务热线：400-806-6969"
-            type="info"
-            :closable="false"
-            description="">
+        <el-alert title="郎牌特曲经销商客服电话：028-86652305、028-86730207" type="info" :closable="false" description="">
+        </el-alert>
+        <el-alert title="全国统一服务热线：400-806-6969" type="info" :closable="false" description="">
         </el-alert>
     </div>
 </template>
@@ -29,7 +20,7 @@ export default {
     components: {},
     data() {
         return {
-            
+
         }
     },
     methods: {
@@ -37,7 +28,7 @@ export default {
             log(data);
         }
     },
-    mounted(){
+    mounted() {
     }
 
 }
